51012afafe8128920f280bbab1a17f296460bcfe
jack <jackfine01@gmail.com>
2025-01-15 07:25:44 -0500

    adding script to index.html

--- javascript.js
+++ javascript.js
@@ -1,16 +1,25 @@
 const myLibrary = [];
 
-function Book(title , author, pages, read) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+
+    this.info = function(){
+        console.log( this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read)
+    }
 };
 
 function addBookToLibrary(Book) {
     myLibrary.push(Book);
 };
 
+function info(Book){
+    return (Book.title + " by " + Book.author + ", " + Book.pages + " pages, " + Book.read)
+};
+
 const Hobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "read.");
 const Nicomachean = new Book("Nicomachean Ethics", "Aristotle", "688", "not read.");
 const Republic = new Book("Politics", "Plato", "408", "not read.");
+
