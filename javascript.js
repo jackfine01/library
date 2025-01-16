ddbb1a5e935cadeb1a7a6b747d2670119fe5232e
jack <jackfine01@gmail.com>
2025-01-16 09:51:09 -0500

    adding intital structure for cards.

--- javascript.js
+++ javascript.js
@@ -16,8 +16,8 @@ function info(Book){
     return (Book.title + " by " + Book.author + ", " + Book.pages + " pages, " + Book.read)
 };
 
-const Hobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "read.");
-const Nicomachean = new Book("Nicomachean Ethics", "Aristotle", "688", "not read.");
+const Hobbit = new Book("Hobbit", "J.R.R. Tolkien", "295", "read.");
+const Ethics = new Book("Ethics", "Aristotle", "688", "not read.");
 const Republic = new Book("Politics", "Plato", "408", "not read.");
 
 addBookToLibrary(Hobbit);
@@ -25,6 +25,16 @@ addBookToLibrary(Nicomachean);
 addBookToLibrary(Republic);
 
 // add display mechanics for html.
+
+const container = document.getElementById("main");
+myLibrary.forEach(Book => {
+    
+    const card = document.createElement("div");
+    card.className = "card";
+    container.appendChild(card);
+
+    card.innerHTML = info(this.Book);
+});
 
 // add library loop function to add to existing display.
 
